b0e219dc2c7e0071631b1f0ef92aa61489b9894d
Matt Clarke <matt.clarke@esss.se>
2020-04-27 07:40:15 +0200

    working

--- epi_judge_python/path_sum.py
+++ epi_judge_python/path_sum.py
@@ -2,8 +2,22 @@ from test_framework import generic_test
 
 
 def has_path_sum(tree, remaining_weight):
-    # TODO - you fill in here.
-    return True
+    def _traverse(root, weight):
+        weight -= root.data
+
+        if weight == 0 and root.left is None and root.right is None:
+            return True
+
+        if root.left:
+            if _traverse(root.left, weight):
+                return True
+        if root.right:
+            if _traverse(root.right, weight):
+                return True
+
+        return False
+
+    return _traverse(tree, remaining_weight)
 
 
 if __name__ == '__main__':
--- epi_judge_python/sum_root_to_leaf.py
+++ epi_judge_python/sum_root_to_leaf.py
@@ -2,26 +2,23 @@ from test_framework import generic_test
 
 
 def sum_root_to_leaf(tree, partial_path_sum=0):
-    def _traverse(root, running_sum, total):
+    def _traverse(root, running_sum):
         if not root:
-            return total
+            return 0
 
         running_sum = (running_sum << 1) + root.data
 
         if not root.left and not root.right:
-            return total + running_sum
+            return running_sum
 
-        if root.left:
-            total = _traverse(root.left, running_sum, total)
-        if root.right:
-            total = _traverse(root.right, running_sum, total)
+        return _traverse(root.left, running_sum) + _traverse(root.right, running_sum)
 
-        return total
-
-    return _traverse(tree, 0, 0)
+    return _traverse(tree, 0)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(
         generic_test.generic_test_main(
-            "sum_root_to_leaf.py", 'sum_root_to_leaf.tsv', sum_root_to_leaf))
+            "sum_root_to_leaf.py", "sum_root_to_leaf.tsv", sum_root_to_leaf
+        )
+    )
